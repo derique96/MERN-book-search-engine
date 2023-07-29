d0e3cab3a413031d770966239012074baf865551
derique96 <fowlkes.derique@gmail.com>
2023-07-29 13:10:10 -0400

    apollo server implemented

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,11 +4,24 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -20,3 +33,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+startApolloServer();
